7312b644d6e6a4f5e9d828f5ab775834fce51040
Maksim <p.m.pavlovich@ya.ru>
2022-11-24 21:25:17 +0300

    Completed task!

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray()
+{
+    Console.WriteLine("Какого размера должен быть наш массив ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    
+    string[] myArray = new string[size];
+
+    for(int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива ");
+        myArray[i] = Console.ReadLine();
+    }
+    return myArray;    
+}
+
+void ShowArray(string[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] NewArray(string[] myArray)
+{
+    string[] newArray = new string[myArray.Length];
+    
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        
+        if (myArray[i].Length <= 3)
+        {
+            newArray[i] = myArray[i];
+        }
+    }
+    return newArray;
+}
+
+string[] myArray = CreateArray();
+Console.Write("Наш изначальный массив: ");
+ShowArray(myArray);
+
+Console.WriteLine();
+
+Console.Write("Результирующий массив: ");
+string[] newArray = NewArray(myArray);
+ShowArray(newArray);
